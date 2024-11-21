edf0d31b8f3f1853711fd8d7fb66d2fb9d8a2c10
Ayanokoji kiyotaka <ayanokojix2306@gmail.com>
2024-11-21 21:58:58 +0100

    Delete routes directory

--- routes/qrRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { generateQR } = require('../controllers/qrController');
-const router = express.Router();
-
-router.get('/generate-qr', generateQR);
-
-module.exports = router;
