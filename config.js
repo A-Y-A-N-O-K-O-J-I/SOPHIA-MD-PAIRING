afb1c3548ac9873ee1ea81c7e721f5a56186336d
Ayanokoji kiyotaka <ayanokojix2306@gmail.com>
2024-11-21 23:25:27 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  SESSION_ID: "", // This will be dynamically updated
+  MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost:27017", // MongoDB URI for connection
+  PORT: process.env.PORT || 3000, // Server Port
+};
