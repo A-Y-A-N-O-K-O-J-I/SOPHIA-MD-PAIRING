661e78accc3e6c304aff1d185e36a2d40aaecffd
Ayanokoji kiyotaka <ayanokojix2306@gmail.com>
2024-11-27 01:04:41 +0100

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "", // This will be dynamically updated
-  MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost:27017", // MongoDB URI for connection
-  PORT: process.env.PORT || 6000, // Server Port
-};
