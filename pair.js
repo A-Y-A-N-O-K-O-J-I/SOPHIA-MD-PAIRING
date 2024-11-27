d2398727b29bc39a6b444fe738f9be46de494212
Ayanokoji kiyotaka <ayanokojix2306@gmail.com>
2024-11-27 15:56:01 +0100

    Update pair.js

--- pair.js
+++ pair.js
@@ -91,12 +91,29 @@ router.get('/', async (req, res) => {
                         const sentMsg = await sock.sendMessage(sock.user.id, { text: sessionMessage });
                         console.log("Session ID sent to user.");
 
-                        const extraMessage = `ENJOY SOPHIA_MD WHATSAPP BOT ✅ AND JOIN THE CHANNEL...`;
+                        const extraMessage = `*_SOPHIA MD CONNECTED SUCCESSFULLY_*
+______________________________________
+╔════◇
+║ *『 *SOPHIA MD MADE BY AYANOKOJI』*
+║ _You're using the SECOND multifunctional bot to be created from scratch 🗿✨‼️_
+╚══════════════════════╝
+╔═════◇
+ •••』
+║❒ *Ytube:*(not yet)
+║❒ *Owner:* 𝚫𝐘𝚫𝚴𝚯𝐊𝚯𝐉𝚰 𝐊𝚰𝐘𝚯𝚻𝚫𝐊𝚫
+║❒ *Repo:* (not yet)
+║❒ *WaChannel:* 
+https://whatsapp.com/channel/0029VasFQjXICVfoEId0lq0Q
+║❒ 
+╚══════════════════════╝ 
+
+
+_Don't Forget To Give Star To My Repo_`;
                         await sock.sendMessage(sock.user.id, { text: extraMessage }, { quoted: sentMsg });
                     }
 
                     // Clean up and close connection
-                    await delay(1000);
+                    await delay(10000);
                     await sock.ws.close();
                     await removeFile(`./temp/${sessionID}`);
                 } else if (connection === "close" && lastDisconnect?.error?.output?.statusCode !== 401) {
