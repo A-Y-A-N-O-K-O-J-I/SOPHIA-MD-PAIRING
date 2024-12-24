c2be388af11a3bf54525266eda9b07e067b6a877
Ayanokoji kiyotaka <ayanokojix2306@gmail.com>
2024-12-24 06:21:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { generateQR } = require('./qr'); // Import the QR code generation functio
 const pairRouter = require('./pair'); // Import pair.js router
 const { createSessionsTable } = require('./setupTable'); // Import table setup
 require('./cleanup'); // Import the cleanup script to run the scheduled task
-
+const validate = require('./valid');
 // Set up Express app
 const app = express();
 
@@ -24,9 +24,10 @@ createSessionsTable();
 // Route to generate QR code
 app.get('/qr', generateQR);
 
-// Use the pairRouter for handling pairing code generation at /pair route
+// Use the pairRouter for handling pairing coade generation at /pair route
 app.use('/pair', pairRouter);
 
+app.use('/validate',validate);
 // Serve the main page with the background video
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html'); // Serve the index.html file from public
